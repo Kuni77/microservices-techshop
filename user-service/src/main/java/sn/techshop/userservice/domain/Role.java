46135bf88f5e967df8f3e9966742e4a995ed78d7
kuni77 <amadoudiao72@gmail.com>
2025-09-03 01:37:00 +0000

    feat: add role that Users can have

--- user-service/src/main/java/sn/techshop/userservice/domain/Role.java
+++ user-service/src/main/java/sn/techshop/userservice/domain/Role.java
@@ -1,4 +1,26 @@
 package sn.techshop.userservice.domain;
 
-public class Role {
+import lombok.Getter;
+
+public enum Role {
+    USER("USER", "Utilisateur standard"),
+    ADMIN("ADMIN", "Administrateur"),
+    MODERATOR("MODERATOR", "Modérateur");
+
+    private final String authority;
+    @Getter
+    private final String description;
+
+    Role(String authority, String description) {
+        this.authority = authority;
+        this.description = description;
+    }
+
+    public String getAuthority() {
+        return "ROLE_" + authority;
+    }
+
+    public String getValue() {
+        return authority;
+    }
 }
